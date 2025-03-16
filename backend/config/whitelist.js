dd4a04fb4b8122188c52c13c4f1852a1f41cdab5
skidev101 <skidev101@gmail.com>
2025-03-16 18:41:46 +0100

    added

--- backend/config/whitelist.js
+++ backend/config/whitelist.js
@@ -1,5 +1,7 @@
 const whitelist = [
-  'https://movize.vercel.app'
+  'https://movize.vercel.app',
+  'https://movize.vercel.app/',
+  'https://movize.vercel.app/search'
 ];
 
 module.exports = whitelist;
