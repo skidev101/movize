5d7ed52130432dcd4872a18b2a6e7f34187236e3
skidev101 <skidev101@gmail.com>
2025-03-21 23:33:08 +0100

    added

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -3,6 +3,7 @@ const whitelist = require('../config/whitelist');
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (whitelist.includes(origin)) {
+    console.log("Passed Middleware")
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
