904a0cf1d333177475a788c44639cbdfe53b20a4
skidev101 <skidev101@gmail.com>
2025-05-27 21:37:45 +0100

    modified server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const corsOptions = require('../config/corsOptions');
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello world');
