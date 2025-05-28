929910bea10f5a22cda4f962666491e49568f0d0
skidev101 <skidev101@gmail.com>
2025-05-28 13:31:05 +0100

    created nowPlaying component

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './Hero.css'
 import HomePage from './HomePage/HomePage'
 import PopularMovies from '../PopularMovies/PopularMovies'
+import NowPlaying from '../NowPlaying/NowPlaying'
+
 
 const Hero = () => {
   return (
     <div className="hero-wrap">
       <HomePage />
       <PopularMovies />
+      <NowPlaying />
     </div>
   )
 }
